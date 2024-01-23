9ae90307c735639541944f20462c0e5a7e8150f7
Esther White <monacodelisa@gmail.com>
2024-01-23 13:25:11 +0200

    styling

--- users-my-rest-api/src/app/app.component.ts
+++ users-my-rest-api/src/app/app.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
 
-import { ApiService } from './services/api.service';
-import { User } from './models/user';
+import { ApiService } from "./services/api.service";
+import { User } from "./models/user";
 
-import {MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, MatCardModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	standalone: true,
+	imports: [
+		CommonModule,
+		RouterOutlet,
+		MatToolbarModule,
+		MatCardModule,
+    MatButtonModule,
+		MatIconModule,
+	],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent implements OnInit {
-  title = 'users-my-rest-api';
-  apiService = inject(ApiService);
-  users: User[] = [];
+	title = "users-my-rest-api";
+	apiService = inject(ApiService);
+	users: User[] = [];
 
-  ngOnInit(): void {
-    this.apiService.getUsers().subscribe((data) => {
-      this.users = data;
-    });
-  }
+	ngOnInit(): void {
+		this.apiService.getUsers().subscribe((data) => {
+			this.users = data;
+		});
+	}
 }
