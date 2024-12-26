3a3b256ebb96d9424bd492dbd24b284ec591f998
Esther White <esthersoftwaredev@gmail.com>
2024-12-26 16:36:04 +0100

    removing annoyiong alert and using conditional rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { User } from "./models/user";
 
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatCardModule } from "@angular/material/card";
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
 import { DomSanitizer } from "@angular/platform-browser";
 
@@ -19,9 +19,9 @@ import { DomSanitizer } from "@angular/platform-browser";
 		CommonModule,
 		MatToolbarModule,
 		MatCardModule,
-    MatMenuModule,
-    MatButtonToggleModule,
-    MatButtonModule,
+		MatMenuModule,
+		MatButtonToggleModule,
+		MatButtonModule,
 		MatIconModule,
 	],
 	templateUrl: "./app.component.html",
@@ -32,23 +32,26 @@ export class AppComponent implements OnInit {
 	apiService = inject(ApiService);
 	users: User[] = [];
 
-  showActiveUsers: boolean = false;
-  showInactiveUsers: boolean = false;
-  showAllUsers: boolean = true;
-  sanitizer = inject(DomSanitizer);
-  matIconRegistry = inject(MatIconRegistry);
-  currentUrl = window.location.href;
+	showActiveUsers: boolean = false;
+	showInactiveUsers: boolean = false;
+	showAllUsers: boolean = true;
+	sanitizer = inject(DomSanitizer);
+	matIconRegistry = inject(MatIconRegistry);
+	currentUrl = window.location.href;
+	linkCopied = false;
 
-  constructor() {
-    this.matIconRegistry.addSvgIcon(
-      "linkedin",
-      this.sanitizer.bypassSecurityTrustResourceUrl("../assets/icons/linkedin.svg")
-    );
-    this.matIconRegistry.addSvgIcon(
-      "x",
-      this.sanitizer.bypassSecurityTrustResourceUrl("../assets/icons/x.svg")
-    );
-  }
+	constructor() {
+		this.matIconRegistry.addSvgIcon(
+			"linkedin",
+			this.sanitizer.bypassSecurityTrustResourceUrl(
+				"../assets/icons/linkedin.svg"
+			)
+		);
+		this.matIconRegistry.addSvgIcon(
+			"x",
+			this.sanitizer.bypassSecurityTrustResourceUrl("../assets/icons/x.svg")
+		);
+	}
 
 	ngOnInit(): void {
 		this.apiService.getUsers().subscribe((data) => {
@@ -56,39 +59,49 @@ export class AppComponent implements OnInit {
 		});
 	}
 
-  showActive() {
-    this.showActiveUsers = true;
-    this.showInactiveUsers = false;
-    this.showAllUsers = false;
-  }
+	showActive() {
+		this.showActiveUsers = true;
+		this.showInactiveUsers = false;
+		this.showAllUsers = false;
+	}
 
-  showInactive() {
-    this.showActiveUsers = false;
-    this.showInactiveUsers = true;
-    this.showAllUsers = false;
-  }
+	showInactive() {
+		this.showActiveUsers = false;
+		this.showInactiveUsers = true;
+		this.showAllUsers = false;
+	}
 
-  showAll() {
-    this.showActiveUsers = false;
-    this.showInactiveUsers = false;
-    this.showAllUsers = true;
-  }
+	showAll() {
+		this.showActiveUsers = false;
+		this.showInactiveUsers = false;
+		this.showAllUsers = true;
+	}
 
-  shareOnLinkedIn() {
-    const linkedInShareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(this.currentUrl)}`;
-    window.open(linkedInShareUrl, '_blank');
-  }
+	shareOnLinkedIn() {
+		const linkedInShareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(
+			this.currentUrl
+		)}`;
+		window.open(linkedInShareUrl, "_blank");
+	}
 
-  shareOnX() {
-    const xShareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(this.currentUrl)}`;
-    window.open(xShareUrl, '_blank');
-  }
+	shareOnX() {
+		const xShareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
+			this.currentUrl
+		)}`;
+		window.open(xShareUrl, "_blank");
+	}
 
-  copyLink() {
-    navigator.clipboard.writeText(this.currentUrl).then(() => {
-      alert('Link copied to clipboard!');
-    }, () => {
-      alert('Failed to copy the link.');
-    });
-  }
+	copyLink() {
+		navigator.clipboard.writeText(this.currentUrl).then(
+			() => {
+				this.linkCopied = true;
+				setTimeout(() => {
+					this.linkCopied = false;
+				}, 2000);
+			},
+			() => {
+				this.linkCopied = false;
+			}
+		);
+	}
 }
